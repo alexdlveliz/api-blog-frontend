6b84d206f62bc50c455cd44b5d977a5aa9e4cda3
NahomiTorres04 <naho_1998-2@outlook.es>
2020-04-21 17:03:28 -0600

    modifique la vista de las categorias

--- src/components/PostMeComida.vue
+++ src/components/PostMeComida.vue
@@ -26,10 +26,25 @@
           <h1></h1>
         </div>
         <div class="content" v-if="index%2==0">
+          <div class="bloguer">
+          <div class="contenedor-bloguer">
+            <div class="cards">
+              <div class="card">
+                <div class="imgBox">
+                  <img src="@/assets/Imagenes-comida/face2.jpg" alt />
+                </div>
+                <div class="contenido-texto-card">
+                  <h4>{{item.user.name}}</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
           <div class="bannerText">
             <div>
               <h2>{{item.title}}</h2>
               <p>{{item.content}}</p>
+              <a href="#">Ver mas</a>
               <a href="#">Comentarios</a>
             </div>
           </div>
@@ -45,11 +60,11 @@
             <div>
               <h2>{{item.title}}</h2>
               <p>{{item.content}}</p>
+              <a href="#">Ver mas</a>
               <a href="#">Comentarios</a>
             </div>
           </div>
-        </div>
-        <div class="bloguer">
+          <div class="bloguer">
           <div class="contenedor-bloguer">
             <div class="cards">
               <div class="card">
@@ -60,17 +75,14 @@
                   <h4>{{item.user.name}}</h4>
                 </div>
               </div>
-              <div class="comentarios">
-                <h1>Haz un comentario</h1>
-                <input type="text" />
-                <a href="#">Enviar</a>
-              </div>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </div>
     <div class="paginacion">
+      <p>¿No encontraste lo que buscabas? sigue explorando</p>
       <button
         :disabled="pagination.prev_page!=null ? false:true"
         @click="getPostsCategory(pagination.prev_page)"
@@ -168,342 +180,398 @@ header {
   position: relative;
   text-align: center;
 }
-nav {
-  text-align: right;
-  padding: 30px 50px 0 0;
+nav{
+    text-align: right;
+    padding: 30px 50px 0 0;   
 }
-nav > a {
-  color: floralwhite;
-  /*font-weight: 300;*/
-  font-family: "Pacifico", cursive;
-  font-size: 18px;
-  text-decoration: none;
-  margin-right: 12px;
+nav > a{
+    color: var(--color-nav);
+    /*font-weight: 300;*/
+    font-family: 'Pacifico', cursive;
+    font-size: 18px;
+    text-decoration: none;
+    margin-right: 12px;
 }
-nav > a:hover {
-  text-decoration: underline;
+nav > a:hover{
+    text-decoration: underline;
 }
-.wave {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
+.wave{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+
 }
-header .textos-header {
-  display: flex;
-  height: 430px;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+header .textos-header{
+    display: flex;
+    height: 430px;
+    width: 100%;align-items: center;
+    justify-content: center;
+    flex-direction: column;
 }
-.textos-header h1 {
-  color: floralwhite;
-  font-size: 50px;
-  font-family: "Rock Salt", cursive;
+.textos-header h1{
+    color: var(--color-nav);
+    font-size: 115px;
+    font-family: 'Rock Salt', cursive;
 }
-.textos-header h2 {
-  color: floralwhite;
-  font-weight: 200;
-  font-size: 25px;
-  font-family: "Roboto Mono", monospace;
+.textos-header h2{
+    color: var(--color-nav);
+    font-weight: 200;
+    font-size: 45px;
+    font-family: 'Roboto Mono', monospace;
 }
 
 /* CONTENIDO-BLOG */
-.otro {
-  padding: 0 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 150px;
+.otro{
+    padding: 0 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 120px;
 }
-.otro h1 {
-  color: var(--color-categoria);
-  text-align: center;
-  font-size: 60px;
-  letter-spacing: 2px;
+.otro h1{
+    color: var(--color-categoria);
+    text-align: center;
+    font-size: 60px;
+    letter-spacing: 2px;
 }
-.container {
-  position: relative;
-  padding: 0 100px;
-  min-height: 100vh;
+.container{
+    position: relative;
+    padding: 0 100px;
+    min-height: 50vh;
 }
-.container:before {
-  content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 450px;
-  height: 100%;
-  background: var(--color-categoria);
-  border-radius: 100% 0% 0% 100%/50% 0% 0% 50%;
-  transform: scaleY(-1);
-  z-index: -2;
+.container:before{
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 325px;
+    height: 100%;
+    background: var(--color-categoria);
+    border-radius: 100% 0% 0% 100%/50% 0% 0% 50%;
+    transform: scaleY(-1);
+    z-index: -2;
 }
 
-.container2 {
-  position: relative;
-  padding: 0 100px;
-  min-height: 100vh;
-  margin-top: 100px;
+.container2{
+    position: relative;
+    padding: 0 100px;
+    min-height: 50vh;
 }
-.container2:before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 450px;
-  height: 100%;
-  background: var(--color-categoria);
-  border-radius: 0% 100% 100% 0%/0% 50% 50% 0%;
-  transform: scaleY(-1);
-  z-index: -2;
+.container2:before{
+    content: '';
+    position: absolute;
+    top: 0;
+    Left: 0;
+    width: 325px;
+    height: 100%;
+    background: var(--color-categoria);
+    border-radius: 0% 100% 100% 0%/0% 50% 50% 0%;
+    transform: scaleY(-1);
+    z-index: -2;
 }
-.container2 .vacio2 {
-  position: relative;
-  width: 420px;
-  height: 100%;
+
+.content{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
 }
-.content {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 100%;
+.content .bannerText{
+    position: relative;
+    max-width: 800px;
+    margin: 15px;
 }
-.content .bannerText {
-  position: relative;
-  max-width: 800px;
+.content .bannerText h2{
+    font-size: 35px;
+    text-transform: uppercase;
+    font-weight: 800;
+    line-height: 1.2em;
+    margin: 10px;
+    color: #333;
 }
-.content .bannerText h2 {
-  font-size: 35px;
-  text-transform: uppercase;
-  font-weight: 800;
-  line-height: 1.2em;
-  margin: 10px;
-  color: #333;
+.content .bannerText p{
+    font-size: 18px;
+    color: #333;
+    line-height: 1.8em;
+    margin: 0;
+    text-align: justify;
 }
-.content .bannerText p {
-  font-size: 18px;
-  color: #333;
-  line-height: 1.8em;
-  margin: 0;
-  text-align: justify;
+.content .bannerText a{
+    position: relative;
+    display: inline-block;
+    text-transform: uppercase;
+    font-weight: 700;
+    color: #333;
+    letter-spacing: 1px;
+    font-size: 10px;
+    text-decoration: none;
+    padding: 15px;
+    margin: 7px;
+    background: var(--color-categoria);
+    border-radius: 5px;
 }
-.content .bannerText a {
-  position: relative;
-  display: inline-block;
-  text-transform: uppercase;
-  font-weight: 700;
-  color: #333;
-  letter-spacing: 1px;
-  font-size: 10px;
-  text-decoration: none;
-  padding: 15px;
-  background: var(--color-categoria);
-  border-radius: 5px;
+.content .bannerImg{
+    position: relative;
+    margin-right: 0px;
+    width: 310px;
+    height: 310px;
+    border-radius: 50%;
 }
-.content .bannerImg {
-  position: relative;
-  margin-right: 25px;
-  width: 440px;
-  height: 440px;
-  border-radius: 50%;
+.content .bannerImg img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+    opacity: 1;
 }
-.content .bannerImg img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
-  object-fit: cover;
-  opacity: 1;
+.content .bloguer{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
 /* Bloguers */
-.container .bloguer,
-.container2 .bloguer {
-  position: relative;
-  width: 100%;
-  height: 250px;
-  margin-top: 60px;
+.container .bloguer, .container2 .bloguer{
+    position: relative;
+    margin-top: 50px;
+    width: 250px;
+    height: 250px;
 }
-.bloguer .contenedor-bloguer {
-  width: 100%;
-  height: 100%;
-  margin-top: 0;
-  margin-left: 0;
-  margin-right: 0;
-  padding: 0;
-  margin-bottom: 0px;
-  display: flex;
-  flex-wrap: wrap-reverse;
+.bloguer .contenedor-bloguer{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
 }
-.contenedor-bloguer .cards {
-  position: relative;
-  margin-top: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap-reverse;
-  min-height: 10vh;
+.bloguer h4{
+    position: relative;
+    color: var(--color-categoria);
 }
-.card {
-  position: relative;
-  width: 250px;
-  height: 250px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 5px 15px;
-  background: #ffffff;
-  transition: 0.5s;
+.contenedor-bloguer .cards{
+    position: relative;
+    margin-top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row-reverse;
+    min-height: 10vh;
 }
-.card:hover {
-  height: 300px;
+.card{
+   position: relative;
+   width: 170px;
+   height: 170px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 5px 15px;
+   background: #ffffff;
+   transition: .50s;
 }
-.card .imgBox {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  padding: 5px;
-  box-sizing: border-box;
+.card:hover{
+    height: 200px;
 }
-.card .imgBox img {
-  max-width: 100%;
-  opacity: 1;
-  transition: 0.5s;
+.card .imgBox{
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   padding: 5px;
+   box-sizing: border-box;
 }
-.card:hover .imgBox img {
-  opacity: 1;
+.card .imgBox img{
+   max-width: 100%;
+   opacity: 1;
+   transition: 0.5s;
 }
-.card:before {
-  content: "";
-  position: absolute;
-  top: -2px;
-  left: -2px;
-  right: -2px;
-  bottom: -2px;
-  background: var(--color-categoria);
-  z-index: -1;
+.card:hover .imgBox img{
+   opacity: 1;
 }
-.card:after {
-  content: "";
-  position: absolute;
-  top: -2px;
-  left: -2px;
-  right: -2px;
-  bottom: -2px;
-  background: var(--color-categoria);
-  z-index: -2;
-  filter: blur(10px);
+.card:before{
+   content: '';
+   position: absolute;
+   top: -2px;
+   left: -2px;
+   right: -2px;
+   bottom: -2px;
+   background: var(--color-categoria);
+   z-index: -1;
 }
-.card:before,
-.card:after {
-  background: var(--gradiente);
+.card:after{
+   content: '';
+   position: absolute;
+   top: -2px;
+   left: -2px;
+   right: -2px;
+   bottom: -2px;
+   background: var(--color-categoria);
+   z-index: -2;
+   filter: blur(10px);
 }
-.card .contenido-texto-card {
-  position: absolute;
-  bottom: 0;
-  left: 10px;
-  right: 10px;
-  bottom: 10px;
-  height: 25px;
-  /* border-radius: 50%; */
-  background: rgba(248, 248, 248, 0.01);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  opacity: 0;
-  transition: 0.5s;
+.card:before, .card:after{
+   background: var(--gradiente);
 }
-.card:hover .contenido-texto-card {
-  opacity: 1;
+.card .contenido-texto-card{
+   position: absolute;
+   bottom: 0;
+   left: 10px;
+   right: 10px;
+   bottom: 10px;
+   height: 25px;
+   /* border-radius: 50%; */
+   background: rgba(248, 248, 248, 0.01);
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   text-align: center;
+   opacity: 0;
+   transition: .5s;
 }
-.card .contenido-texto-card h4 {
-  font-size: 25px;
-  color: black;
-  font-family: "Pacifico", cursive;
-  line-height: 20px;
-  letter-spacing: 2px;
+.card:hover .contenido-texto-card{
+   opacity: 1;
 }
-/* BOTONES */
-.iconos {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 110px;
-  margin: 5px;
-  padding: 25px;
+.card .contenido-texto-card h4{
+   font-size: 17px;
+   color: black;
+   font-family: 'Pacifico', cursive;
+   line-height: 20px;
+   letter-spacing: 2px;
 }
-.iconos .controladores {
-  position: relative;
-  display: flex;
-  flex-direction: row;
+/* Hacer comentarios */
+.cards .comentarios{
+    position: relative;
+    width: 600px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
 }
-.controladores a {
-  position: relative;
-  display: inline-block;
-  text-transform: uppercase;
-  font-weight: 700;
-  color: #333;
-  letter-spacing: 1px;
-  font-size: 12px;
-  text-decoration: none;
-  padding: 10px;
-  margin: 0 7px;
-  background: var(--color-categoria);
-  border-radius: 5px;
+.comentarios h1{
+    position: relative;
+    color: black;
+    font-size: 25px;
+    margin: 5px;
+}
+.comentarios input{
+    position: relative;
+    text-align: justify;
+    width: 98%;
+    height: 100%;
+    margin: 5px;
+    padding: 5px;
+    border: 1px solid var(--color-categoria);
+}
+.comentarios a{
+    width: 100px;
+    height: 35px;
+    background: var(--color-categoria);
+    color: #ffffff;
+    text-align: center;
+    border-radius: 5px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    margin-right: 5px;
+    margin-left: 460px;
+    text-transform: uppercase;
+    font-weight: 700;
+    color: #333;
+    letter-spacing: 1px;
+    font-size: 12px;
+    text-decoration: none;
+    padding: 10px;
+}
+/* ver comentarios */
+.flex{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.comentario-card{
+    display: none;
+    position: fixed;
+    z-index: 1;
+    overflow: auto;
+    left: 0;
+    top: 0px;
+    width: 100%;
+    background: rgba(245, 241, 3, 0.24);
+}
+.contenido-modal{
+    position: relative;
+    background: #ffffff;
+    border-radius: 25px;
+    margin: 150px auto;
+    width: 50%;
+    box-shadow: 0 0 6px 0 rgba(248, 248, 248, 0.4);
+    animation-name: modal;
+    animation-duration: 1s;
+}
+@keyframes modal{
+    from{top: -330px; opacity: 0;}
+    to{top:0px; opacity: 1;}
+}
+.close{
+    color: white;
+    font-size: 30px;
+    font-weight: bold;
+}
+.close:hover{
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+.modal-header, .modal-footer{
+    padding: 8px 16px;
+    display: flex;
+    justify-content: space-between;
+    background: var(--color-categoria);
+}
+.modal-body{
+    position: relative;
+    padding: 20px 16px;
+    display: flex;
+    flex-direction: column;
+}
+.modal-body .comentario-cont{
+    position: relative;
+    margin: 5px;
+}
+.comentario-cont h3{
+    text-align: left;
+}
+.comentario-cont p{
+    text-align: justify;
+    letter-spacing: 1px;
 }
 
-/* comentarios */
-.cards .comentarios {
-  position: relative;
-  width: 600px;
-  height: 250px;
-  display: flex;
-  flex-direction: column;
-}
-.comentarios h1 {
-  position: relative;
-  color: black;
-  font-size: 25px;
-  margin: 5px;
-}
-.comentarios input {
-  position: relative;
-  text-align: justify;
-  width: 98%;
-  height: 100%;
-  margin: 5px;
-  padding: 5px;
-  border: 1px solid var(--color-categoria);
-}
-.comentarios a {
-  width: 100px;
+/* Botones de paginacion */
+.paginacion{
+  width: 100%;
   height: 35px;
-  background: var(--color-categoria);
-  color: #ffffff;
-  text-align: center;
-  border-radius: 5px;
-  margin-top: 5px;
-  margin-bottom: 5px;
-  margin-right: 5px;
-  margin-left: 460px;
-  text-transform: uppercase;
-  font-weight: 700;
-  color: #333;
-  letter-spacing: 1px;
-  font-size: 12px;
-  text-decoration: none;
-  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .paginacion button{
+  text-decoration: none;
+  color: #333;
+  font-size: 12px;
+  text-transform: uppercase;
+  font-weight: bold;
+  padding: 5px;
+  margin: 8px;
+  border-radius: 7px;
+  box-shadow: none;
   background: var(--color-categoria);
+}
+.paginacion button:hover{
+  cursor: pointer;
 }
 .portafolio {
   margin-top: 45px;
@@ -521,6 +589,7 @@ header .textos-header {
   font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
   font-size: 20px;
 }
+/* CATEGORIAS */
 /*---------------Galeria-Categorías---------------*/
 .contenedor-categorias {
   margin: 0;
